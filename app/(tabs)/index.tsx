d05f70d0ccbb437365ea7fffd8ff602b423c17ba
DanAraya <gedan100@gmail.com>
2024-06-19 12:14:43 -0600

    SitemateChallenge: Comments removed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,20 @@
-import { Image, StyleSheet, Platform, Button } from 'react-native';
+import { Image, StyleSheet, Platform, Button } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import useFetchData from '../api/hooks/useFetchData';
-import { useEffect } from 'react';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import useFetchData from "../api/hooks/useFetchData";
+import { useEffect } from "react";
 
 export default function HomeScreen() {
-
-
-  // const setData =() => {
-  //   console.log(useFetchData().data)
-  // }
-  // useEffect(()=>{
-  //  const { data } = useFetchData();
-  //  console.log(data)
-  // })
+  useEffect(() => {
+    const { data } = useFetchData();
+    console.log(data);
+  });
 
   const { data } = useFetchData();
-  console.log(data)
+  console.log(data);
   return (
     <ThemedView style={styles.wrapContainer}>
       <ThemedView style={styles.titleContainer}>
@@ -28,20 +23,19 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         {/* <Button
-  onPress={setData}
-  title="Learn More"
-  color="#841584"
-  accessibilityLabel="Learn more about this purple button"
-/> */}
+        onPress={setData}
+        title="Get Data"
+        color="#841584"
+      /> */}
       </ThemedView>
-      </ThemedView>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -51,11 +45,4 @@ const styles = StyleSheet.create({
   wrapContainer: {
     marginTop: 48,
   },
-  // reactLogo: {
-  //   height: 178,
-  //   width: 290,
-  //   bottom: 0,
-  //   left: 0,
-  //   position: 'absolute',
-  // },
 });
